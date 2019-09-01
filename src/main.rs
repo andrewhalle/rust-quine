7687d0694ad63e5e8e5099b8e9a12b145f14e013
Andrew Halle <ahalle@berkeley.edu>
2019-09-01 11:57:01 -0700

    really close

---
+++ src/main.rs
@@ -0,0 +1,17 @@
+fn main() {
+    let program = "fn main() {\n    let program = \"Z\";\n\n    for c in program.chars() {\n        if c as u8 == 90 {\n            for d in program.chars() {\n                match d as u8 {\n                    10 => print!(\"\\\\n\"),\n                    34 => print!(\"\\\\\\\"\"),\n                    _ => print!(\"{}\", d),\n                }\n            }\n        } else {\n            print!(\"{}\", c);\n        }\n    }\n}\n";
+
+    for c in program.chars() {
+        if c as u8 == 90 {
+            for d in program.chars() {
+                match d as u8 {
+                    10 => print!("\\n"),
+                    34 => print!("\\\""),
+                    _ => print!("{}", d),
+                }
+            }
+        } else {
+            print!("{}", c);
+        }
+    }
+}

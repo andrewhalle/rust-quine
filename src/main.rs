a9aacef4d806a1272aa741ae57acee51b11b1c93
Andrew Halle <ahalle@berkeley.edu>
2019-09-01 12:06:34 -0700

    successful quine

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 fn main() {
-    let program = "fn main() {\n    let program = \"Z\";\n\n    for c in program.chars() {\n        if c as u8 == 90 {\n            for d in program.chars() {\n                match d as u8 {\n                    10 => print!(\"\\\\n\"),\n                    34 => print!(\"\\\\\\\"\"),\n                    _ => print!(\"{}\", d),\n                }\n            }\n        } else {\n            print!(\"{}\", c);\n        }\n    }\n}\n";
+    let program = "fn main() {\n    let program = \"Z\";\n\n    for c in program.chars() {\n        if c as u8 == 90 {\n            for d in program.chars() {\n                match d as u8 {\n                    10 => print!(\"\\\\n\"),\n                    34 => print!(\"\\\\\\\"\"),\n                    92 => print!(\"\\\\\\\\\"),\n                    _ => print!(\"{}\", d),\n                }\n            }\n        } else {\n            print!(\"{}\", c);\n        }\n    }\n}\n";
 
     for c in program.chars() {
         if c as u8 == 90 {
@@ -7,6 +7,7 @@ fn main() {
                 match d as u8 {
                     10 => print!("\\n"),
                     34 => print!("\\\""),
+                    92 => print!("\\\\"),
                     _ => print!("{}", d),
                 }
             }
